d40f0bcb5ea7af9458952f5dce2b64b6a6b33b0c
kyky <kyle.vuanh@gmail.com>
2025-04-11 22:31:58 +0700

    [CREATE] LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,54 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import loginUser from '../apis/loginUser';
+
 export default function LoginPage() {
-    return 'This is Login Page';
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        try {
+            localStorage.setItem('token', await loginUser(username, password));
+            setError('');
+            navigate(`/`);
+        } catch (err) {
+            setError(err);
+        }
+    }
+    return (
+        <div className='flex items-center justify-center h-screen'>
+            <form onSubmit={handleLogin} className='bg-white p-6 rounded shadow-lg'>
+                <div>
+                    <label className='block font-medium'>Username</label>
+                    <input 
+                        type='text' 
+                        value={username} 
+                        onChange={(text) => setUsername(text.target.value)} 
+                        className='p-2 border rounded mt-1 w-full'
+                        placeholder='Enter your username'                         
+                        required 
+                    />
+                </div>
+                <div>
+                    <label className='block font-medium'>Password</label>
+                    <input 
+                        type='password' 
+                        value={password} 
+                        onChange={(text) => setPassword(text.target.value)} 
+                        className='p-2 border rounded mt-1 w-full'
+                        placeholder='Enter your password'                           
+                        required 
+                    />
+                </div>
+                {error && <p className='text-red-500'>{error}</p>}
+                <button 
+                    type='submit' 
+                    className='w-full py-2 px-4 bg-blue-500 text-black font-semibold rounded mt-4'
+                    >Submit
+                </button>
+            </form>
+        </div>
+    );
 }
